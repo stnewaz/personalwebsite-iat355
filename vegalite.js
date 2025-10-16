b9b56ac5c5c0bcca7b137021833bb0b856f7d9cb
stnewaz <youremail@example.co>
2025-10-15 20:08:12 -0700

    final edits

--- vegalite.js
+++ vegalite.js
@@ -408,7 +408,7 @@ function createNintendoLineChart(containerId) {
         { "aggregate": "sum", "field": "global_sales", "title": "Sales (millions)" }
       ]
     },
-    "width": "contain",
+    "width": 3400,
     "height": 1200,
     "autosize": { "type": "fit-x", "resize": true, "contains": "padding" },
     "title": {
